cae0390cdb283f8be88f55f3b00b2724f1814b13
Larissa-P-B <126874430+Larissa-P-B@users.noreply.github.com>
2026-03-10 21:19:08 -0300

    Serviço Publicado

--- src/main/java/br/com/fiap/winery/WineStockServiceImplementation.java
+++ src/main/java/br/com/fiap/winery/WineStockServiceImplementation.java
@@ -2,15 +2,23 @@ package br.com.fiap.winery;
 
 import jakarta.jws.WebService;
 
-@WebService(endpointInterface = "br.com.fiap.WineStockService")
+@WebService(endpointInterface = "br.com.fiap.winery.WineStockService")
 public class WineStockServiceImplementation implements WineStockService{
     @Override
     public String getMenu() {
-        return "";
+       return "===== MENU DE VINHOS =====\n"
+                + "Cabernet Sauvignon - Vinho tinto encorpado\n"
+                + "Merlot - Vinho tinto suave\n"
+                + "Pinot Noir - Vinho tinto leve\n"
+                + "Chardonnay - Vinho branco seco\n"
+                + "Sauvignon Blanc - Vinho branco aromático\n"
+                + "Malbec - Vinho tinto intenso\n";
+
+
     }
 
     @Override
     public String placeOrder(String name, int quantity) {
-        return "";
+        return "Pedido recebido: " + quantity + " garrafas de " + name;
     }
 }
